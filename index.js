18daac398f4a144c86f96fb71263f1f5b7981d00
Villain <83833885+rajibhasanrikto71@users.noreply.github.com>
2022-04-05 22:28:33 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+x = document.querySelector("title");
+a = document.querySelector(".but1");
+b = document.querySelector(".but2");
+c = document.querySelector(".but3");
+d = document.querySelector(".but4");
+
+x.innerHTML="R R R";
+a.innerHTML="কোরআন শিখি";
+b.innerHTML="নামাজ শিখি";
+c.innerHTML="গুগোল শিট শিখি";
+d.innerHTML="ডেভেলপমেন্ট";
