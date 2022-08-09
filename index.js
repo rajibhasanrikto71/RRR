b0564f2c7730dce4fc7cbcc547f340a226cba34f
Villain <83833885+rajibhasanrikto71@users.noreply.github.com>
2022-08-09 23:59:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ a.innerHTML="Apps";
 b.innerHTML="F T P";
 c.innerHTML="Eboardresults";
 d.innerHTML="Train Ticket";
-e.innerHTML="Download Facebook Video";
+e.innerHTML="Download FB Video";
 f.innerHTML="Help Centre";
 
